e4611ca99cbcc28ee1b868c8e49f2b6caed386f1
Alexandre Martos <alexandre.martos@protonmail.ch>
2020-05-26 14:23:21 +0200

    Fix crash when export in freeze --noconsole

--- ymael/core/export/panexport.py
+++ ymael/core/export/panexport.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import pypandoc
+import subprocess
 import os
 import shutil
 import logging
@@ -8,6 +8,7 @@ logger = logging.getLogger(__name__)
 
 
 from .markdown import MDMaker
+from .noconsole_subprocess import subprocess_args
 
 
 class PanExporter:
@@ -17,7 +18,10 @@ class PanExporter:
         return [".pdf",".odt",".docx",".md"]
 
     def __init__(self, filename, rps):
-        logger.info("Pandoc version: {}".format(pypandoc.get_pandoc_version()))
+        self._subprocess_args = subprocess_args(False)
+        version = self.get_pandoc_version()
+        logger.info("Pandoc version: {}".format(version))
+
         filename, ext = os.path.splitext(filename)
         if not ext in self.supported_extensions():
             logger.warning("Output format %s not supported. Switching to .md", ext)
@@ -41,15 +45,38 @@ mainfont: FreeSans
             tmp_file += ".md"
         MDMaker(tmp_file, rps, add_before)
         logger.debug("File is located at {}".format(tmp_file))
-        if ext != ".md":
-            try:
-                pypandoc.convert_file(tmp_file, ext.lstrip("."), format="markdown",
-                        outputfile=filename,
-                        extra_args=['--pdf-engine', 'xelatex'])
-                os.remove(tmp_file)
-            except OSError:
-                logger.warning("Pandoc is not installed.")
-            except RuntimeError:
-                logger.warning("No latex distribution available.")
-            except AttributeError:
-                logger.exception("Conversion error.")
+        if version and ext != ".md":
+            self.convert_file(tmp_file, filename)
+
+    def get_pandoc_version(self):
+        try:
+            version = subprocess.check_output(
+                    ["pandoc", "--version"],
+                    **self._subprocess_args
+                    )
+            version = version.decode("utf-8").split()[1]
+            return version
+        except FileNotFoundError:
+            logger.warning("Pandoc is not installed.")
+            return None
+
+    def convert_file(self, input_file, output_file):
+        command = [
+                "pandoc",
+                "-f","markdown+smart",
+                "-o",output_file,
+                "--pdf-engine", "xelatex",
+                "--verbose",
+                input_file
+                ]
+        try:
+            out = subprocess.check_output(
+                    command,
+                    **self._subprocess_args
+                    )
+            out = out.decode("utf-8")
+            logger.debug(out)
+            os.remove(input_file)
+            logger.info("Conversion successful.")
+        except subprocess.CalledProcessError:
+            logger.exception("Conversion error.")
